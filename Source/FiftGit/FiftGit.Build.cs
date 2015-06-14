adcf617a6401ef9308e7fa07065c800a261a7c8a
smallB <atch.cpp@gmail.com>
2015-06-14 12:59:32 +0100

    Source code files
    Those are the source code files which I've started with during creating
    new project in UE4

---
+++ Source/FiftGit/FiftGit.Build.cs
@@ -0,0 +1,11 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class FiftGit : ModuleRules
+{
+	public FiftGit(TargetInfo Target)
+	{
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+	}
+}
---
+++ Source/FiftGit/FiftGit.h
@@ -0,0 +1,9 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __FIFTGIT_H__
+#define __FIFTGIT_H__
+
+#include "EngineMinimal.h"
+
+
+#endif
